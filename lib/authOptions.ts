0661c3a2358ac588d94fdf866f5851f8587a6689
Anshik-02 <anshik091@gmail.com>
2025-03-08 17:01:15 +0530

    final checks

--- lib/authOptions.ts
+++ lib/authOptions.ts
@@ -5,12 +5,31 @@ import { userModel } from "@/model/schema";
 import { JWT } from "next-auth/jwt";
 
 export const authOptions: NextAuthOptions = {
+
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID ?? "",
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
     }),
   ],
+  cookies: {
+    sessionToken: {
+      name: `__Secure-next-auth.session-token`,
+      options: {
+        httpOnly: true,
+        secure: process.env.NEXTAUTH_URL?.startsWith('https://'),
+        sameSite: 'lax',
+        path: '/',
+      },
+    },
+  },
   callbacks: {
     async session({ session, token }: { session: any; token: JWT }) {
       if (token.id) {
